a89e0b98da5cca03b18c3803878e86567b829048
Vysakh-vn <vysakhvn28@gmail.com>
2025-01-26 17:02:50 +0530

    More updation for skills container

--- src/components/SkillsComponent.vue
+++ src/components/SkillsComponent.vue
@@ -7,24 +7,67 @@
         <div class="skills-container">
             <div class="shadow rounded">
                 <img :src="require('@/assets/java.svg')">
+                <p>Java</p>
             </div>
             <div class="shadow rounded">
                 <img :src="require('@/assets/springboot.svg')">
+                <p>Spring Boot</p>
             </div>
             <div class="shadow rounded">
                 <img :src="require('@/assets/rest-api-icon.svg')">
+                <p>REST API</p>
             </div>
             <div class="shadow rounded">
                 <img :src="require('@/assets/mysql-official.svg')">
+                <p>MySQL</p>
             </div>
             <div class="shadow rounded">
                 <img :src="require('@/assets/html-1.svg')">
+                <p>Html</p>
             </div>
             <div class="shadow rounded">
                 <img :src="require('@/assets/css-3.svg')">
+                <p>CSS</p>
             </div>
             <div class="shadow rounded">
                 <img :src="require('@/assets/aws-2.svg')">
+                <p>AWS</p>
+            </div>
+            <div class="shadow rounded">
+                <img :src="require('@/assets/javascript-1.svg')">
+                <p>Javascript</p>
+            </div>
+            <div class="shadow rounded">
+                <img :src="require('@/assets/bootstrap-5-1.svg')">
+                <p>Bootstrap</p>
+            </div>
+            <div class="shadow rounded">
+                <img :src="require('@/assets/vue-9.svg')">
+                <p>Vue js</p>
+            </div>
+            <div class="shadow rounded">
+                <img :src="require('@/assets/figma-icon.svg')" style="width: 50px;">
+                <p>Figma</p>
+            </div>
+            <div class="shadow rounded">
+                <img :src="require('@/assets/git-icon.svg')">
+                <p>Git</p>
+            </div>
+            <div class="shadow rounded">
+                <img :src="require('@/assets/github.svg')">
+                <p>Github</p>
+            </div>
+            <div class="shadow rounded">
+                <img :src="require('@/assets/postman.svg')">
+                <p>Postman</p>
+            </div>
+            <div class="shadow rounded">
+                <img :src="require('@/assets/gradle-1.svg')">
+                <p>Gradle</p>
+            </div>
+            <div class="shadow rounded">
+                <img :src="require('@/assets/apache-maven-1.svg')">
+                <p>Maven</p>
             </div>
         </div>
 
@@ -34,14 +77,21 @@
 <style>
 
 .skills-container div {
-    display: flex;
+    display: grid;
+    grid-template-rows: 3fr 1fr;
     justify-content: center;
+    align-items: center;
+    color: #818181;
+    font-size: 0.8rem;
+    font-weight: 500;
+    background-color: white;
 }
 
 .skills-container img {
     width: 70px;
-    /* padding: 1rem; */
-    margin: 1rem;
+    margin: 0.5rem;
+    margin-right: 2rem;
+    margin-left: 2rem;
 }
 
 .skills-container {
