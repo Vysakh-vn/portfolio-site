6662bdcf8e6001b5886edbcb16dd6c17d0a10bbf
Vysakh-vn <vysakhvn28@gmail.com>
2025-01-30 19:47:00 +0530

    Added alt tag of the images and Updated skills order

--- src/components/SkillsComponent.vue
+++ src/components/SkillsComponent.vue
@@ -6,68 +6,64 @@
 
         <div class="skills-container">
             <div class="shadow rounded">
-                <img :src="require('@/assets/java.svg')">
+                <img :src="require('@/assets/java.svg')" alt="Java logo">
                 <p>Java</p>
             </div>
             <div class="shadow rounded">
-                <img :src="require('@/assets/springboot.svg')">
+                <img :src="require('@/assets/springboot.svg')" alt="Spring boot logo">
                 <p>Spring Boot</p>
             </div>
             <div class="shadow rounded">
-                <img :src="require('@/assets/rest-api-icon.svg')">
+                <img :src="require('@/assets/rest-api-icon.svg')" alt="REST logo">
                 <p>REST API</p>
             </div>
             <div class="shadow rounded">
-                <img :src="require('@/assets/mysql-official.svg')">
+                <img :src="require('@/assets/mysql-official.svg')" alt="MySQL logo">
                 <p>MySQL</p>
             </div>
             <div class="shadow rounded">
-                <img :src="require('@/assets/html-1.svg')">
+                <img :src="require('@/assets/html-1.svg')" alt="Html logo">
                 <p>Html</p>
             </div>
             <div class="shadow rounded">
-                <img :src="require('@/assets/css-3.svg')">
+                <img :src="require('@/assets/css-3.svg')" alt="CSS logo">
                 <p>CSS</p>
             </div>
             <div class="shadow rounded">
-                <img :src="require('@/assets/aws-2.svg')">
-                <p>AWS</p>
-            </div>
-            <div class="shadow rounded">
-                <img :src="require('@/assets/javascript-1.svg')">
+                <img :src="require('@/assets/javascript-1.svg')" alt="Javascript logo">
                 <p>Javascript</p>
             </div>
             <div class="shadow rounded">
-                <img :src="require('@/assets/bootstrap-5-1.svg')">
+                <img :src="require('@/assets/bootstrap-5-1.svg')" alt="Bootstrap logo">
                 <p>Bootstrap</p>
             </div>
             <div class="shadow rounded">
-                <img :src="require('@/assets/vue-9.svg')">
+                <img :src="require('@/assets/vue-9.svg')" alt="Vue js logo">
                 <p>Vue js</p>
             </div>
             <div class="shadow rounded">
-                <img :src="require('@/assets/figma-icon.svg')" style="width: 50px;">
+                <img :src="require('@/assets/aws-2.svg')" alt="AWS logo">
+                <p>AWS</p>
+            </div>
+            <div class="shadow rounded">
+                <img :src="require('@/assets/figma-icon.svg')" style="width: 50px;" alt="Figma logo">
                 <p>Figma</p>
             </div>
             <div class="shadow rounded">
-                <img :src="require('@/assets/git-icon.svg')">
+                <img :src="require('@/assets/git-icon.svg')" alt="Git logo">
                 <p>Git</p>
             </div>
             <div class="shadow rounded">
-                <img :src="require('@/assets/github.svg')">
+                <img :src="require('@/assets/github.svg')" alt="Github logo">
                 <p>Github</p>
             </div>
             <div class="shadow rounded">
-                <img :src="require('@/assets/postman.svg')">
+                <img :src="require('@/assets/postman.svg')" alt="Postman logo">
                 <p>Postman</p>
             </div>
             <div class="shadow rounded">
-                <img :src="require('@/assets/gradle-1.svg')">
+                <img :src="require('@/assets/gradle-1.svg')" alt="Gradle logo">
                 <p>Gradle</p>
-            </div>
-            <div class="shadow rounded">
-                <img :src="require('@/assets/apache-maven-1.svg')">
-                <p>Maven</p>
             </div>
         </div>
 
