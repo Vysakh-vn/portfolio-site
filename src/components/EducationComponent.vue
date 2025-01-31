0431605282da85d6514c19a4d6911aa2a70c73aa
Vysakh-vn <vysakhvn28@gmail.com>
2025-01-31 13:49:35 +0530

    Added Education details

--- src/components/EducationComponent.vue
+++ src/components/EducationComponent.vue
@@ -4,7 +4,19 @@
     </div>
     <div class="d-flex justify-content-center">
         <div class="education-container col-10 rounded shadow">
-
+            <div class="text-container">
+                <p>2018 - 2022</p>
+                <p>B.tech (Electronics and Communications)</p>
+                <p>College of Engineering, Muttathara</p>
+                <br><br>
+                <p>2016 - 2018</p>
+                <p>Higher Secondary</p>
+                <p>St Joseph,s boys school, Trivandrum</p>
+                <br><br>
+                <p>2004 - 2016</p>
+                <p>High School</p>
+                <p>Lourdes Mount school, Trivandrum</p>
+            </div>
         </div>
     </div>
 </template>
