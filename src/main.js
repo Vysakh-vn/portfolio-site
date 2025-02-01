21d01aefb571b8a545357c740292a4c5007ce5c7
Vysakh-vn <vysakhvn28@gmail.com>
2025-02-01 15:41:59 +0530

    Removed unnessary files

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
-createApp(App).mount('#app')
